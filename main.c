f9ac434e6f7ab9d8382ba8003652875a6c70c90d
Liam Westby <lw322@nau.edu>
2013-03-10 12:44:41 -0700

    Initial commit.

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  flatdb
+//
+//  Created by Liam Westby on 3/10/13.
+//  Copyright (c) 2013 Me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+
