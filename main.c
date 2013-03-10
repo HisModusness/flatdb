d2ea8f0c8976380a70b91e9f77faad0b4149f1dc
Liam Westby <lw322@nau.edu>
2013-03-10 14:09:20 -0700

    Added test for db_add().

--- main.c
+++ main.c
@@ -8,14 +8,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "flatdb.h"
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    Person *people = (Person *)malloc(sizeof(Person)*5);
+    people[0].id = 1;
+    people[0].name = "Bill Gates";
+    
+    people[1].id = 2;
+    people[1].name = "Steve Jobs";
+    
+    people[2].id = 3;
+    people[2].name = "Richard Stallman";
+    
+    people[3].id = 4;
+    people[3].name = "Dennis Ritchie";
+    
+    people[4].id = 5;
+    people[4].name = "Balls Shepard";
+    
+    int fd = open("/Users/lw322/Documents/db.txt", O_RDWR | O_CREAT | O_APPEND);
+    
+    if (fd > 2) {
+        db_add(fd, &people[0]);
+        db_add(fd, &people[1]);
+        db_add(fd, &people[2]);
+        db_add(fd, &people[3]);
+        db_add(fd, &people[4]);
+    }
+    
+    close(fd);
+    
+    
     return 0;
 }
 
